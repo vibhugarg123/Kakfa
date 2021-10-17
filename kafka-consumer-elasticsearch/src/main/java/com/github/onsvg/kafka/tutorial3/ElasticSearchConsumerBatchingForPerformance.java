cf18fc86ec0075a262401cf33ec39c4c6b6c250b
Vibhu Garg <g.vibhu05@gmail.com>
2021-10-18 00:23:21 +0530

    [Consumer] Batching records into bulk request for elastic search

---
+++ kafka-consumer-elasticsearch/src/main/java/com/github/onsvg/kafka/tutorial3/ElasticSearchConsumerBatchingForPerformance.java
@@ -0,0 +1,136 @@
+package com.github.onsvg.kafka.tutorial3;
+
+import com.google.gson.JsonParser;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ElasticSearchConsumerBatchingForPerformance {
+
+    public static final Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+    private static final JsonParser jsonParser = new JsonParser();
+
+    public static RestHighLevelClient createClient() {
+
+        String hostname = "kafka-course-5043681523.eu-west-1.bonsaisearch.net";
+        String username = "4mal98k12e";
+        String password = "zrqx8nn96r";
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+    public static KafkaConsumer<String, String> createKafkaConsumer(String topic) {
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elasticsearch";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+        //Disable Auto-Commit of offsets
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        //subscribe consumer to our topic(s)
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+
+        return consumer;
+    }
+
+
+    private static String extractIdFromTweet(String tweetJson) {
+        return jsonParser.parse(tweetJson)
+                .getAsJsonObject()
+                .get("id_str")
+                .getAsString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = createClient();
+
+        KafkaConsumer<String, String> consumer = createKafkaConsumer("twitter_tweets");
+
+        while (true) {
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100));// timeout of 100 millis
+            Integer recordCount = records.count();
+            logger.info("Received " + recordCount + " records");
+
+            BulkRequest bulkRequest = new BulkRequest();
+            for (ConsumerRecord<String, String> record : records) {
+                try {
+                    //Extract is from tweetJson for making idempotent consumer
+                    //Make it as twitter feed specific id
+                    String id = extractIdFromTweet(record.value());
+
+                    //insert data into elastic search
+
+                    //It will fail if twitter index does not exist
+                    IndexRequest indexRequest = new IndexRequest(
+                            "twitter",
+                            "tweets",
+                            id
+                    ).source(record.value(), XContentType.JSON);
+
+                    bulkRequest.add(indexRequest);// we add to our bulk request (takes no time)
+                } catch (NullPointerException e) {
+                    logger.warn("skipping bad data: {}", record.value());
+                }
+            }
+
+            if (recordCount > 0) {
+                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                logger.info("Committing offsets...");
+                consumer.commitSync();
+                logger.info("Offsets have been committed");
+                try {
+                    Thread.sleep(1000); //introduce a small delay
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //close the client gracefully
+        //client.close();
+    }
+}
